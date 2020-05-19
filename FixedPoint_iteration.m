94e8d26a8003555deb1d51f1895e42a5c75151ef
RamezMaher <45771210+ramezmaher@users.noreply.github.com>
2020-05-20 00:03:59 +0200

    Add files via upload

--- FixedPoint_iteration.m
+++ FixedPoint_iteration.m
@@ -1,4 +1,4 @@
-function [iterations, root ,error ,Fx] = FixedPoint_iteration(f,g,n,t,x)
+function [iterations, root ,error ,Fx] = FixedPoint_iteration(f,g,n,t,p)
 % Equation root finder
 %   finds the root of an equation by using the fixed-poiny iteration method
 root = zeros(1,n);
@@ -6,12 +6,14 @@ iterations = zeros(1,n);
 error = zeros(1,n);
 Fx = zeros(1,n);
 i = 1;
-X_old=x;
+X_old=p;
+gx = str2func(g);
+fx = str2func(f);
 while i<=n
     iterations(i)=i;
-    X_new = feval(g,X_old);
+    X_new = feval(gx,X_old);
     root(i) = X_new;
-    Fx(i) = feval(f,X_new);
+    Fx(i) = feval(fx,X_new);
     if(X_new ~= 0)
         error(i) = (X_new-X_old)/X_new;
     end
